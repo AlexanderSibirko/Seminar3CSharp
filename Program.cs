7cd0d2f96acf5ac4e9a87ef8eec89369e69bec34
Alexander <Alexandersibirko@yandex.ru>
2021-11-06 12:13:18 +0300

    added task 4

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@
 int SquareNum(int N)
 { return N * N; }
 
-int Num = new Random().Next(-50, 50);
-Console.WriteLine($"Число {Num} в квадрате = {SquareNum(Num)}");
+int Num0 = new Random().Next(-50, 50);
+Console.WriteLine($"Число {Num0} в квадрате = {SquareNum(Num0)}");
 
 //  1. По двум заданным числам проверять является ли первое квадратом второго
 // bool aSqrbYesNo(int Na, int Nb)
@@ -12,11 +12,21 @@ Console.WriteLine($"Число {Num} в квадрате = {SquareNum(Num)}");
 // int a = 16;
 // int b = 4;
 // Console.WriteLine($"Число {a} является квадратом числа {b}? Ответ: {aSqrbYesNo(a, b)}");
-
+int a1 = 1;
+int b1 = 0;
+bool SQR(int a, int b)
+{
+    if (a == b * b)
+    {
+        return true;
+    }
+    return false;
+}
+Console.WriteLine($"{SQR(a1, b1)} a: {a1} , b: {b1} ");
 
 //  2. Даны два числа. Показать большее и меньшее число
-int a = new Random().Next(-50, 500);
-int b = new Random().Next(-50, 500);
+int a2 = new Random().Next(-50, 500);
+int b2 = new Random().Next(-50, 500);
 void BiggerSmaller(int a, int b)
 {
     if (a < b)
@@ -28,7 +38,7 @@ void BiggerSmaller(int a, int b)
         Console.WriteLine($"min = {b}, max = {a}");
     }
 }
-BiggerSmaller(a, b);
+BiggerSmaller(a2, b2);
 
 //  3. По заданному номеру дня недели вывести его название
 // void DayWeekName (int daynumber)
@@ -43,20 +53,38 @@ BiggerSmaller(a, b);
 int number = 4;
 void DayWeek(int number)
 {
-    if (number == 1) Console.WriteLine ("понедельник");
-    if (number == 2) Console.WriteLine ("вторник");
-    if (number == 3) Console.WriteLine ("среда");
-    if (number == 4) Console.WriteLine ("четверг");
-    if (number == 5) Console.WriteLine ("пятница");
-    if (number == 6) Console.WriteLine ("суббота");
-    if (number == 7) Console.WriteLine ("воскресенье");
+    if (number == 1) Console.WriteLine("понедельник");
+    if (number == 2) Console.WriteLine("вторник");
+    if (number == 3) Console.WriteLine("среда");
+    if (number == 4) Console.WriteLine("четверг");
+    if (number == 5) Console.WriteLine("пятница");
+    if (number == 6) Console.WriteLine("суббота");
+    if (number == 7) Console.WriteLine("воскресенье");
 }
 DayWeek(number);
 
-
 //  4. Найти максимальное из трех чисел
-//  5. Написать программу вычисления значения функции y=f(a)
+int a4 = new Random().Next(-50,50);
+int b4 = new Random().Next(-50,50);
+int c4 = new Random().Next(-50,50);
+Console.WriteLine($"a={a4}, b={b4}, c={c4}");
+
+int Max(int a, int b, int c)
+{
+    int result = a;
+    if (b > result) result = b;
+    if (c > result) result = c;
+    return result;
+}
+int max = Max(a4, b4, c4);
+Console.WriteLine($"Максимальное число = {max}");
+
+//  5. Написать программу вычисления значения функции y=f(a), где f(а) = sin^[кол-во букв Фамилии](а)
+
+
 //  6. Выяснить является ли число чётным
+
+
 //  7. Показать числа от -N до N
 int N = 20;
 void ShowNums(int N)
@@ -68,20 +96,37 @@ void ShowNums(int N)
     Console.WriteLine();
 }
 ShowNums(N);
-//  8. Показать четные числа от 1 до И
-//  9. Показать последнюю цифру трёхзначного числа
-// 10. Показать вторую цифру трёхзначного числа
-// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
-// 12. Удалить вторую цифру трёхзначного числа
-// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
-// 14. Найти третью цифру числа или сообщить, что её нет
-int Number = new Random().Next(-100000, 100000);
 
-string FindFirdNumber(int N)
-{
-    N = N / 100;
-    string NumStr = Convert.ToString(N);
-    return NumStr + " Проверка";
-        // return "Третей цифры нет";
-}
-Console.WriteLine(FindFirdNumber(Number));
+//  8. Показать четные числа от 1 до И
+
+
+//  9. Показать последнюю цифру трёхзначного числа
+
+
+// 10. Показать вторую цифру трёхзначного числа
+
+
+// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+
+// 12. Удалить вторую цифру трёхзначного числа
+
+
+// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+
+// 14. Найти третью цифру числа или сообщить, что её нет
+// int Number = new Random().Next(-100000, 100000);
+// Console.WriteLine(Number);
+// string FindFirdNumber(int N)
+// {
+//     string NumStr = Convert.ToString(N);
+//     if (N < 0)
+//     {
+//         NumStr = Right(NumStr, NumStr.Length-1);
+//     }
+
+//     return NumStr + " Проверка";
+//     // return "Третей цифры нет";
+// }
+// Console.WriteLine(FindFirdNumber(Number));
