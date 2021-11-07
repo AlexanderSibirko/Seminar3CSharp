2bdc3914dc510bd4e98f4996c3b120dbcf737c27
Alexander <Alexandersibirko@yandex.ru>
2021-11-07 16:40:05 +0300

    task 17 code added

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -1,1 +1,13 @@
 ﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+int a; 
+int b;
+bool aSqrbYesNo(int Na, int Nb) => Na == Math.Pow(Nb,2) || Nb == Math.Pow(Na,2);
+
+a = 6; b = 5;
+Console.WriteLine($"Является ли одно из чисел {a},{b} квадратом другого числа? Ответ: {aSqrbYesNo(a, b)}");
+a = -2; b = 4;
+Console.WriteLine($"Является ли одно из чисел {a},{b} квадратом другого числа? Ответ: {aSqrbYesNo(a, b)}");
+a = 9; b = 3;
+Console.WriteLine($"Является ли одно из чисел {a},{b} квадратом другого числа? Ответ: {aSqrbYesNo(a, b)}");
+a = -9; b = 4;
+Console.WriteLine($"Является ли одно из чисел {a},{b} квадратом другого числа? Ответ: {aSqrbYesNo(a, b)}");
