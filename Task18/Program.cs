2e1f7c0d623166d2885c0315b875a4824fe90eca
Alexander <Alexandersibirko@yandex.ru>
2021-11-07 18:49:37 +0300

    task 18 solved?

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,1 +1,27 @@
 ﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+// Проверка на "листочке"
+// Исх   Лев Ч   Прав Ч
+// 0/0     1       1
+// 0/1     0       0
+// 1/0     0       0
+// 1/1     0       0
+// Вывод должны получить в итоге true
+
+bool solveTester()
+{
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            bool x = Convert.ToBoolean(i);
+            bool y = Convert.ToBoolean(j);
+            if (!((!(x || y)) == (!x && !y)))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+Console.WriteLine(solveTester());
