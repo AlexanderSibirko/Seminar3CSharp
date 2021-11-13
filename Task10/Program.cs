2e3e5abc32b00790eb579e8e96e88a75b54fd375
Alexander <Alexandersibirko@yandex.ru>
2021-11-13 21:38:17 +0300

    added prefomance tests for tasks 10,11

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -8,7 +8,7 @@ void SecondNumber(int a)
 {
     a = a % 100;
     a = a / 10;
-    Console.WriteLine($"Вторая цифра числа: {a}");
+    //Console.WriteLine($"Вторая цифра числа: {a}");
 }
 SecondNumber(a10);
 
@@ -20,5 +20,20 @@ int SecondNum(int a)
 Console.WriteLine($"Вторая цифра числа: {SecondNum(a10)}");
 
 // Альтернатива 2
-int SecondNum2(int a) => (a % 100) / 10; //или так
+
+int SecondNum2(int a) => (a / 10) % 10; //или так
+int SecondNum3(int a) => (a % 100) / 10; //или так 
+
 Console.WriteLine($"Вторая цифра числа: {SecondNum2(a10)}");
+Console.WriteLine($"Вторая цифра числа: {SecondNum3(a10)}");
+
+// // Мерить лучше по очереди, при одновременном тесте, всегда быстрее выполняется второе решение ?видимо из-за каких-то кэширований?
+// DateTime dt = DateTime.Now;
+// for (int i = 0 ; i < 1_000_000; i++)
+// {SecondNum2(a10);}
+// Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+// dt = DateTime.Now;
+// for (int i = 0 ; i < 1_000_000; i++)
+// {SecondNum3(a10);}
+// Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
