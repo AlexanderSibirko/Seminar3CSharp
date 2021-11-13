0cd1166b0842211a259cd220dc74dc072da4871d
Alexander <Alexandersibirko@yandex.ru>
2021-11-14 00:13:09 +0300

    added comments task22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,5 +1,5 @@
 ﻿// 22. Найти расстояние между точками в пространстве 2D/3D
-string printPointArr(double[] arr)
+string printPointArr(double[] arr) //печать массива точки
 {
     string result ="(";
     for (int i=0;i < arr.Length;i++)
@@ -9,9 +9,9 @@ string printPointArr(double[] arr)
     return result.TrimEnd(' ', ',')+")";
 }
 
-double DoubleRndNum (double min, double max) => new Random().NextDouble()*(max-min)+min;
+double DoubleRndNum (double min, double max) => new Random().NextDouble()*(max-min)+min; //произвольное действительно число между Мин и Макс 
 
-double[] createPoint (int dimensionsNum)
+double[] createPoint (int dimensionsNum) //создаём массив координат точки
 {
     double[] point = new double[dimensionsNum];
     for (int i=0; i < point.Length; i++)
@@ -35,19 +35,19 @@ double distUniveral(double[] point1, double[] point2) //точки должны 
     return value = Math.Sqrt(value);
 }
 
-double[] point1_2D = createPoint(2);
-double[] point2_2D = createPoint(2);
-Console.WriteLine(printPointArr(point1_2D));
-Console.WriteLine(printPointArr(point2_2D));
-Console.WriteLine(dist2D(point1_2D,point2_2D));
-Console.WriteLine(distUniveral(point1_2D,point2_2D));
+double[] point1_2D = createPoint(2); //создаем точку 1
+double[] point2_2D = createPoint(2); //создаем точку 2
+Console.WriteLine(printPointArr(point1_2D)); //координаты точки 1
+Console.WriteLine(printPointArr(point2_2D)); //координаты точки 2
+Console.WriteLine(dist2D(point1_2D,point2_2D)); //расстояние методом только для двумерного пространства
+Console.WriteLine(distUniveral(point1_2D,point2_2D)); //используя универсальный метод для точек любой мерности
 
-double[] point1_3D = createPoint(3);
-double[] point2_3D = createPoint(3);
-Console.WriteLine(printPointArr(point1_3D));
-Console.WriteLine(printPointArr(point2_3D));
-Console.WriteLine(dist3D(point1_3D,point2_3D));
-Console.WriteLine(distUniveral(point1_3D,point2_3D));
+double[] point1_3D = createPoint(3); //создаем точку 1
+double[] point2_3D = createPoint(3); //создаем точку 2
+Console.WriteLine(printPointArr(point1_3D)); //координаты точки 1
+Console.WriteLine(printPointArr(point2_3D)); //координаты точки 2
+Console.WriteLine(dist3D(point1_3D,point2_3D)); //расстояние методом только для трёхмерного пространства
+Console.WriteLine(distUniveral(point1_3D,point2_3D)); //используя универсальный метод для точек любой мерности
 
 // point1_2D[0] = 0;
 // point1_2D[1] = 10;
