cb1b6076c632a4c987417968f3deb37463c99fca
Alexander <Alexandersibirko@yandex.ru>
2021-11-13 22:15:51 +0300

    made better test part for task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,9 +2,9 @@
 Console.WriteLine(); //лишняя красота
 Console.WriteLine("Задача 13: Выяснить, кратно ли число заданному, если нет, вывести остаток."); //лишняя красота
 
-int a13 = new Random().Next(-100, 100);
-int k13 = new Random().Next(0, 16);
-Console.WriteLine($"Число {a13} кратно ли {k13}? Если нет какой остаток?");
+// int a13 = new Random().Next(-20, 21);
+// int k13 = new Random().Next(2, 5);
+
 
 string ifmultiply(int N, int K)
 {
@@ -15,4 +15,11 @@ string ifmultiply(int N, int K)
     }
     return result;
 }
+
+int a13 = 15;
+int k13 = 5;
+Console.WriteLine($"Число {a13} кратно ли {k13}? Если нет какой остаток?");
+Console.WriteLine(ifmultiply(a13, k13));
+k13 = 8;
+Console.WriteLine($"Число {a13} кратно ли {k13}? Если нет какой остаток?");
 Console.WriteLine(ifmultiply(a13, k13));
