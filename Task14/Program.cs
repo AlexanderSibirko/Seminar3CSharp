c11315ab67e4d128fffb6f9f817cc514f6d35a00
Alexander <Alexandersibirko@yandex.ru>
2021-11-13 23:23:02 +0300

    added no string conversionf for task14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -23,9 +23,38 @@ string FindThirdNumber(int N, bool fromRtoL = true) //ищем третью ци
     }
     return result;
 }
+
+
 int Number14 = new Random().Next(-100, 100);
 Console.WriteLine($"Тест для слишком короткого числа. {FindThirdNumber(Number14)}");
 Number14 = new Random().Next(-10000000, 10000000);
 Console.WriteLine($"Ищем третье справа-налево по разрядам, для числа {Number14}. {FindThirdNumber(Number14)}");
 Number14 = new Random().Next(-10000000, 10000000);
 Console.WriteLine($"Ищем третье слева-направо \"как читаем\", для числа {Number14}. {FindThirdNumber(Number14, false)}");
+
+
+//Вариант того же метода без приведения к строкам
+int thirdNuminNumber (int N, bool RtoL = true) //возварат -1, означает что цифры нету
+{
+    N = Math.Abs(N); //убираем знак для поиска третьего числа
+    if (N / 100 == 0) {return -1;}
+    if (RtoL) 
+    {
+        return N % 1000 / 100;
+    }
+    else
+    {
+        for (int i = 0; N > 999; i++) 
+        {
+            N /= 10;
+        }
+        return N % 10;
+    }
+}
+
+Number14 = new Random().Next(-100, 100);
+Console.WriteLine($"Тест для слишком короткого числа. {thirdNuminNumber(Number14)}");
+Number14 = new Random().Next(-10000000, 10000000);
+Console.WriteLine($"Ищем третье справа-налево по разрядам, для числа {Number14}. {thirdNuminNumber(Number14)}");
+Number14 = new Random().Next(-10000000, 10000000);
+Console.WriteLine($"Ищем третье слева-направо \"как читаем\", для числа {Number14}. {thirdNuminNumber(Number14, false)}");
