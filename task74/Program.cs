e5b6398a6eb4fe05dc14bd3857e9130db67bd983
Alexander <Alexandersibirko@yandex.ru>
2021-12-10 20:31:33 +0300

    task 74

--- task74/Program.cs
+++ task74/Program.cs
@@ -4,9 +4,60 @@
 // int n = 3;
 
 
-// string allWords(char[] aiChars, int maxWordLength)
+// int combinations(int alphLen, int maxWordLen)
 // {
-    
+//     if (maxWordLen == 0) return 1;
+//     else if (maxWordLen % 2 == 0) return combinations(alphLen * alphLen, maxWordLen / 2);
+//     else return combinations(alphLen, maxWordLen - 1) * alphLen;
 // }
 
-// Console.WriteLine(allWords);
+// // Console.WriteLine(combinations(alphLen, 1));
+// // Console.WriteLine(combinations(alphLen, 2));
+// // Console.WriteLine(combinations(alphLen, 3));
+// // Console.WriteLine(combinations(alphLen, 4));
+
+// string[] allWords(char[] letters, int maxWordLength) //delimitered by ' '
+// {
+//     for (int i = 0; i <= maxWordLength; i++)
+//     {
+
+//     }
+// // }
+
+
+//с Интернетов =) https://stackoverflow.com/a/24757048    2014 год
+IEnumerable<String> GenerateStrings (IEnumerable<char> characters, int length) {
+    if(length > 0) {
+        foreach(char c in characters) {
+            foreach(String suffix in GenerateStrings(characters,length-1)) {
+                yield return c+suffix;
+            }
+        }
+    } else {
+        yield return string.Empty;
+    }
+}
+
+char[] aiLetters = { 'a', 'b', 'c', 'd' };
+int maxWordLen = 3;
+foreach(string aiword in GenerateStrings(aiLetters,maxWordLen))
+{
+    Console.WriteLine(aiword);
+}
+
+
+//с лекции Сергея
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet,word,length+1);
+    }
+}
+
+FindWords("abcd", new char[3]);
